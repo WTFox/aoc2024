65c6038c16fd0dfd8eb98abf05d7abc762470c54
A. Fox <anthonyfox1988@gmail.com>
2024-12-01 08:10:16 -0800

    init

---
+++ src/days/day01.rs
@@ -0,0 +1,18 @@
+pub fn part_one(input: &str) -> i32 {
+    0
+}
+
+pub fn part_two(input: &str) -> i32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one_example_one() {
+        let result = part_one("");
+        assert_eq!(result, 0);
+    }
+}
