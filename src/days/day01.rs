5742f0feb89bf0070eee22d22c40a5357cbb90ac
A. Fox <anthonyfox1988@gmail.com>
2024-12-01 08:43:29 -0800

    1.2

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,5 +1,7 @@
-pub fn part_one(input: &str) -> i32 {
-    let (mut left, mut right): (Vec<i32>, Vec<i32>) = input
+use std::collections::HashMap;
+
+fn process_input(input: &str) -> Vec<(i32, i32)> {
+    input
         .lines()
         .map(|line| {
             if line.is_empty() {
@@ -8,8 +10,11 @@ pub fn part_one(input: &str) -> i32 {
             let mut nums = line.split_whitespace().map(|n| n.parse::<i32>().unwrap());
             (nums.next().unwrap(), nums.next().unwrap())
         })
-        .unzip();
+        .collect()
+}
 
+pub fn part_one(input: &str) -> i32 {
+    let (mut left, mut right): (Vec<i32>, Vec<i32>) = process_input(input).iter().cloned().unzip();
     left.sort();
     right.sort();
 
@@ -19,15 +24,30 @@ pub fn part_one(input: &str) -> i32 {
         .sum()
 }
 
-pub fn part_two(_input: &str) -> i32 {
-    0
+pub fn part_two(input: &str) -> i32 {
+    let (mut left, mut right): (Vec<i32>, Vec<i32>) = process_input(input).iter().cloned().unzip();
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    left.sort();
+    right.sort();
+
+    left.iter()
+        .map(|l| {
+            if let Some(v) = map.get(l) {
+                return *v;
+            }
+            let count = right.iter().filter(|&r| r == l).count();
+            let product = *l * (count as i32);
+            map.insert(*l, product);
+            product
+        })
+        .sum()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const input: &str = "3   4
+    const INPUT: &str = "3   4
 4   3
 2   5
 1   3
@@ -35,8 +55,14 @@ mod tests {
 3   3";
 
     #[test]
-    fn test_part_one_example_one() {
-        let result = part_one(input);
+    fn test_part_one() {
+        let result = part_one(INPUT);
         assert_eq!(result, 11);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(INPUT);
+        assert_eq!(result, 31);
     }
 }
